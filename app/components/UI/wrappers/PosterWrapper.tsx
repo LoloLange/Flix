1144f222509d36c7132e489ef31385679308f161
LoloLange <lololange08@gmail.com>
2025-02-23 11:56:26 -0300

    fix: fixed small screens responsive errors

--- app/components/UI/wrappers/PosterWrapper.tsx
+++ app/components/UI/wrappers/PosterWrapper.tsx
@@ -2,7 +2,7 @@ import useWindowSize from "@/app/hooks/useWindowSize";
 import { usePathname } from "next/navigation";
 import { ReactNode } from "react";
 
-export const PosterWrapper = ({ children, poster, extraClassName }: { children?: ReactNode, poster: string, extraClassName?: string }) => {
+export const PosterWrapper = ({ children, poster, extraClassName, noMask }: { children?: ReactNode, poster: string, extraClassName?: string; noMask?: boolean }) => {
   const windowSize = useWindowSize();
   const pathname = usePathname();
   return (
@@ -16,7 +16,7 @@ export const PosterWrapper = ({ children, poster, extraClassName }: { children?:
         backgroundPosition: "center",
         boxSizing: "border-box",
         boxShadow: windowSize.width && windowSize.width >= 800 ? "inset 200px 50px 500px 200px rgba(0, 0, 0, 0.6)" : "inset 500px 500px 500px 500px rgba(0, 0, 0, 0.65)",
-        maskImage: "linear-gradient(black 60%, transparent)",
+        maskImage: !noMask ? "linear-gradient(black 60%, transparent)" : "none",
       }}
     >{children}</div>
   );
--- app/components/home/RecentMovies.tsx
+++ app/components/home/RecentMovies.tsx
@@ -14,10 +14,10 @@ export const RecentMovies = ({
 }) => {
   const current = recent[0];
   return (
-    <section className="py-5 min-[700px]:py-10 min-[1000px]:py-20">
-      <PosterWrapper poster={poster} extraClassName="px-5 z-10">
+    <section className="py-10 min-[700px]:py-20 min-[1000px]:py-32 max-[400px]:mb-20">
+      <PosterWrapper poster={poster} extraClassName="px-5" noMask={true}>
         <RecentMoviesInfo current={current} tvShows={tvShows} />
-        <div className="min-[450px]:absolute bottom-14 w-[calc(100%-50px)] min-[600px]:mx-auto">
+        <div className="min-[450px]:absolute bottom-10 min-[650px]:bottom-0 w-[calc(100%-50px)] min-[600px]:mx-auto">
           <SwiperContainer results={recent} tvShows={tvShows ? true : false} />
         </div>
       </PosterWrapper>
